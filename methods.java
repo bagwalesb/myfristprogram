9455ff4b3cbe0dbc17acc00d76ba69bf5f927aaa
bagwalesb <106956959+bagwalesb@users.noreply.github.com>
2022-06-27 18:47:20 +0530

    Add files via upload

---
+++ methods.java
@@ -0,0 +1,17 @@
+public class methods {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ methods m=new methods();
+ m.Sum();
+}
+		
+	
+	void Sum() 
+	{
+		System.out.print("learn coading");
+	 
+ }
+	}
+
+
